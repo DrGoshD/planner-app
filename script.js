69aecb594f1f9de06cd3f026a65de75b7c8fda1c
DrPeppr <md.purcaru@gmail.com>
2023-03-18 19:18:28 +0000

    function to get current day

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const container = $('.container');
 const timeBlocks = $('.time-block');
 const saveBtn = $('.saveBtn i');
 
+
+
+const today = moment();
+$('#currentDay').text(today.format("dddd, Do MMMM YYYY"));
